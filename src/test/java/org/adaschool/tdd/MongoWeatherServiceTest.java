990af6f973f97d34e9a524d44db881078b3ae647
Eduard Arias <eduard.arias@mail.escuelaing.edu.co>
2021-09-16 04:57:56 -0400

    Part 3 completed with some test cases

--- src/test/java/org/adaschool/tdd/MongoWeatherServiceTest.java
+++ src/test/java/org/adaschool/tdd/MongoWeatherServiceTest.java
@@ -13,10 +13,13 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -66,6 +69,47 @@ class MongoWeatherServiceTest
         String weatherReportId = "dsawe1fasdasdoooq123";
         when( repository.findById( weatherReportId ) ).thenReturn( Optional.empty() );
         Assertions.assertThrows( WeatherReportNotFoundException.class, () -> weatherService.findById( weatherReportId ));
+    }
+
+    @Test
+    void shouldGetAllReportsOfReporter()
+    {
+        String reporter = "Eduard";
+        List<WeatherReport> reports = new ArrayList<>();
+        reports.add(new WeatherReport(new GeoLocation(10.0, 20.0), 10, 2.3, reporter, new Date()));
+        reports.add(new WeatherReport(new GeoLocation(10.0, 40.0), 30, 0.2, reporter, new Date()));
+
+        when(repository.findByReporter(reporter)).thenReturn(reports);
+        Assertions.assertEquals(reports, weatherService.findWeatherReportsByName(reporter));
+
+    }
+
+    @Test
+    void shouldIgnoreNullReporter()
+    {
+        when(repository.findByReporter(nullable(String.class))).thenReturn(new ArrayList<>());
+        List<?> reports = weatherService.findWeatherReportsByName(null);
+        Assertions.assertNotNull(reports);
+        Assertions.assertFalse(reports.size() > 0);
+
+        verify(repository).findByReporter(nullable(String.class));
+    }
+
+    @Test
+    void shouldGetNearbyGeolocations()
+    {
+        GeoLocation geoLocation = new GeoLocation(20.1, 4.555);
+        List<WeatherReport> reports = new ArrayList<>();
+        reports.add(new WeatherReport(geoLocation, 10.22, 4.09, "This guy", new Date()));
+        reports.add(new WeatherReport(geoLocation, 32.22, 9.0, "This guy", new Date()));
+        reports.add(new WeatherReport(new GeoLocation(10.1, 0), 2,2,"222", new Date()));
+
+        List<WeatherReport> reportClone = new ArrayList<>();
+        reportClone.addAll(reports);
+
+        when(repository.findAll()).thenReturn(reportClone);
+        reports.remove(2);
+        Assertions.assertEquals(reports, weatherService.findNearLocation(geoLocation, 0));
     }
 
 }
